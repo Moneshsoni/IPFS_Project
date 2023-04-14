36675446750122df15decc4d554afa5afa678c1f
Moneshsoni <sonimonesh115@gmail.com>
2023-04-14 16:57:25 +0530

    add the project setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+function App(){
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello this</h1>
+        <p>This is My Application for store Files on IPFS</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
